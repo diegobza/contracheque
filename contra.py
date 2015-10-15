41a3d1a6f68d335f89d8724f8acca2c4d77d6669
Diego Bezerra <diego@tatulab.com>
2015-10-15 15:58:57 -0300

    Formatação monetária.

--- contra.py
+++ contra.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from escpos import *
+import locale
 import xml.etree.ElementTree as ET
 
 
@@ -13,14 +14,33 @@ def cpf(cpf):
     return "%s.%s.%s-%s" % (cpf[0:3], cpf[3:6], cpf[6:9], cpf[9:11])
 
 
+def real(valor):
+    v_float = float(valor)
+    v_real = locale.currency(v_float, True, True)
+    return v_real
+
+
 def dict_contra(codigo):
     switcher = {
-        199: 'Vantagens Diversas'
+        199: "VANTAGENS DIVERSAS",
+        201: "PREVIDÊNCIA (INSS)",
+        202: "PREVIDÊNCIA (REGIME PRÓPRIO)",
+        203: "IMPOSTO DE RENDA RETIDO NA FONTE (IRRF)",
+        299: "DESCONTOS DIVERSOS"
     }
     return switcher.get(codigo, "nothing")
 
-Epson = printer.Usb(0x04b8, 0x0e03)
-Epson.charcode('portuguese')
+
+def p_item(codigo, valor):
+    print dict_contra(codigo)
+    if codigo >= 201:
+        valor = -valor
+    print locale.currency(float(valor), False, True)
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+'''Epson = printer.Usb(0x04b8, 0x0e03)
+Epson.charcode('portuguese')'''
 
 tree_f = ET.parse('folha.xml')
 root = tree_f.getroot()
@@ -29,7 +49,7 @@ folha = root.find('folhaPagamento')
 contra = folha.find('contracheques')
 
 for i in contra:
-    if i.attrib.get('matricula') == '00000002':
+    if i.attrib.get('matricula') == '00000001':
         print i.attrib
         '''Epson.set('center')
         Epson.image('logo-lajes.png')
@@ -45,6 +65,6 @@ for i in contra:
         p('MATRICULA:  ' + i.attrib.get('matricula') + '\n')'''
         for j in i:
             codigo = j.attrib.get('codigo')
-            print codigo
-            print dict_contra(codigo)
+            valor = j.attrib.get('valor')
+            p_item(int(codigo), float(valor))
         '''Epson.cut()'''
