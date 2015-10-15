febaa8baecba0f421079b119c912cbf19e657f84
Diego Bezerra <diego@tatulab.com>
2015-10-15 19:58:18 -0300

    Melhorias diversas.

--- contra.py
+++ contra.py
@@ -14,33 +14,60 @@ def cpf(cpf):
     return "%s.%s.%s-%s" % (cpf[0:3], cpf[3:6], cpf[6:9], cpf[9:11])
 
 
-def real(valor):
-    v_float = float(valor)
-    v_real = locale.currency(v_float, True, True)
-    return v_real
-
-
 def dict_contra(codigo):
     switcher = {
         199: "VANTAGENS DIVERSAS",
-        201: "PREVIDÊNCIA (INSS)",
-        202: "PREVIDÊNCIA (REGIME PRÓPRIO)",
+        201: "PREVIDENCIA (INSS)",
+        202: "PREVIDENCIA (REGIME PROPRIO)",
         203: "IMPOSTO DE RENDA RETIDO NA FONTE (IRRF)",
         299: "DESCONTOS DIVERSOS"
     }
     return switcher.get(codigo, "nothing")
 
 
-def p_item(codigo, valor):
-    print dict_contra(codigo)
-    if codigo >= 201:
-        valor = -valor
-    print locale.currency(float(valor), False, True)
+def p_items(itens):
+    total = 0
+    for i in itens:
+        cod = int(i[0])
+        val = float(i[1])
+
+        if cod >= 201:
+            val = -val
+
+        total += val
+
+        f_cod = dict_contra(cod)
+        f_val = locale.currency(val, False, True)
+
+        espac = 48 - (len(f_cod) + len(f_val))
+        print espac
+
+        if espac > 0:
+            Epson.set('left')
+            p(f_cod + espac * ' ' + f_val + '\n')
+
+    Epson.set('right')
+    p('-------\n')
+    Epson.set('right', '', 'b')
+    f_total = locale.currency(total, True, True)
+    p('TOTAL  ' + f_total + '\n')
+
+
+def intro():
+    Epson.set('center')
+    Epson.image('logo-lajes.png')
+    Epson.set('center', '', '', '', 2)
+    p('PREFEITURA MUNICIPAL DE LAJES\n')
+    Epson.set('center')
+    p('CNPJ: 08.113.466/0001-05\n')
+    Epson.set('center', '', 'b')
+    p('\nDADOS DO SERVIDOR\n\n')
+
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-'''Epson = printer.Usb(0x04b8, 0x0e03)
-Epson.charcode('portuguese')'''
+Epson = printer.Usb(0x04b8, 0x0e03)
+Epson.charcode('portuguese')
 
 tree_f = ET.parse('folha.xml')
 root = tree_f.getroot()
@@ -50,21 +77,16 @@ contra = folha.find('contracheques')
 
 for i in contra:
     if i.attrib.get('matricula') == '00000001':
-        print i.attrib
-        '''Epson.set('center')
-        Epson.image('logo-lajes.png')
-        Epson.set('center', '', '', '', 2)
-        p('PREFEITURA MUNICIPAL DE LAJES\n')
-        Epson.set('center')
-        p('CNPJ: 08.113.466/0001-05\n')
-        Epson.set('center', '', 'b')
-        p('\nDADOS DO SERVIDOR\n\n')
+        intro()
         Epson.set('left')
         p('NOME:       ' + i.attrib.get('nome') + '\n')
         p('CPF:        ' + cpf(i.attrib.get('cpf')) + '\n')
-        p('MATRICULA:  ' + i.attrib.get('matricula') + '\n')'''
+        p('MATRICULA:  ' + i.attrib.get('matricula') + '\n')
+        Epson.set('center', '', 'b')
+        p('\nCONTRACHEQUE\n\n')
+        itens = []
         for j in i:
-            codigo = j.attrib.get('codigo')
-            valor = j.attrib.get('valor')
-            p_item(int(codigo), float(valor))
-        '''Epson.cut()'''
+            item = [j.attrib.get('codigo'), j.attrib.get('valor')]
+            itens.append(item)
+        p_items(itens)
+        Epson.cut()
